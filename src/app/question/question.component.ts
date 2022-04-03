7dde834f9b083650b7dacd6fdf528866e677cc89
komalsonar8 <komalsonar8@gmail.com>
2022-04-03 22:24:35 +0530

    previous and nex button coding

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../service/question.service';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
-import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/observable/throw';
-
+// import { Observable } from 'rxjs';
+//import 'rxjs/add/operator/map';
+// import { HttpClient } from '@angular/common/http';
+// import 'rxjs/add/observable/throw';
+// import { Observable, of, Subject } from 'rxjs';
+// import { mapTo } from "rxjs/operators";
+// const answer = of(6 * 9).pipe(mapTo(42));
 
 @Component({
   selector: 'app-question',
@@ -14,6 +16,10 @@ import 'rxjs/add/observable/throw';
 export class QuestionComponent implements OnInit {
 
   public name:string="";
+  public questionList:any=[];
+  public currentQuestion:number=0;
+  public points : number=0;
+  counter=60;
   constructor(private questionService : QuestionService) { }
 
   ngOnInit(): void {
@@ -25,8 +31,26 @@ export class QuestionComponent implements OnInit {
   getAllQuestions(){
     this.questionService.getQuestionJson()
     .subscribe((res: any)=>{
-      console.log(res.questions);
+      this.questionList=res.questions;
     })
+  }
+
+  nextQuesion(){
+    this.currentQuestion++;
+
+  }
+
+  previousQuestion(){
+
+    this.currentQuestion--;
+  }
+
+  answer(currentQno:number , option:any){
+
+    if(option.correct){
+      this.points+=10;
+      //this.points=this.points+10;
+    }
   }
 
 }
--- src/app/service/question.service.ts
+++ src/app/service/question.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import 'rxjs/add/observable/throw';
-import { catchError } from 'rxjs/operators';
+//import { Observable } from 'rxjs';
+// import 'rxjs/add/observable/throw';
+// import { catchError } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
 
+
+//import { mapTo } from "rxjs/operators";
+//const answer = of(6 * 9).pipe(mapTo(42));
 @Injectable({
   providedIn: 'root'
 })
